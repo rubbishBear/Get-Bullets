aa9400ff47f2c8a96ffc0af137519fc30cfc8fd9
rubbishBear <36151339+rubbishBear@users.noreply.github.com>
2025-03-09 13:55:31 -0400

    Update Get-Bullets.ps1

--- Get-Bullets.ps1
+++ Get-Bullets.ps1
@@ -2,11 +2,11 @@
 .SYNOPSIS
         Use this script to generate your five bullets for the previous weeks achievements.
 .DESCRIPTION
-        This PowerShell will take a text document containing a list of bullet achievements as input and will randomly pick 5 to use as your previous weeks achievements.
+        This PowerShell script will take a text document containing a list of bullet achievements as input and will randomly pick 5 to use as your previous weeks achievements.
 .EXAMPLE
-        PS> Gen-Bullets.ps1 -Bullets <bullets list>.txt
+        PS C:\>Get-Bullets.ps1 -Bullets <bullets list>.txt
 .LINK
-        https://www.github.com/rubbishBear/Gen-Bullets
+        https://www.github.com/rubbishBear/Get-Bullets
 .NOTES
         Author: rubbishBear
 #>
@@ -15,17 +15,17 @@ param (
     [array]$Bullets
 )
 
-if (-Not (Test-Path -Path $bullets)) {
-    Write-Host "The file does not exist: $bullets"
+if (-Not (Test-Path -Path $Bullets)) {
+    Write-Host "The file does not exist: $Bullets"
     exit
 }
 
-if (-Not ($bullets -match '\.txt$')) {
+if (-Not ($Bullets -match '\.txt$')) {
     Write-Host "File format not recognized."
     exit
 }
 
-$randomFive = Get-Content $bullets | Get-Random -Count 5
+$randomFive = Get-Content $Bullets | Get-Random -Count 5
 
 [int]$bulletNumber = "1"
 
